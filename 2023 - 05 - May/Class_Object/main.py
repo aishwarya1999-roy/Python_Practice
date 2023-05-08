ee4723f01bc245436960fb95bc813d1b044ae9ca
Aishwarya <aishwarya99roy@gmail.com>
2023-05-08 23:46:24 +0530

    Update

--- 2023 - 05 - May/Class_Object/main.py
+++ 2023 - 05 - May/Class_Object/main.py
@@ -55,59 +55,64 @@ banana.details()"""
 my_Guiter = guiter()"""
 
 
-#inheritance
+# inheritance
 
-#parent class
-class Guiter :
+# parent class
+"""class Guiter :
     def __init__(self, n_string) :
         self.n_string = n_string  # attribute
         self.play()
         self.__cost = 50
+
     def play(self) :  # self parameter inside the new function makes it a method
-        print("Pa para parappa pappa pa pa pa ")
+        print("I from Parent class ")
 
 
-
-
-#child class
-class ElectricGuiter(Guiter): #inheriting everything from Guiter class
-    def __init__(self): # changing something from parent class
-        super().__init__() #allowing the subclass to override methods or attributes of parent class and provide its own implementation.
-        self.n_string = 8
-        self.color = ("#000000", "#FFFFFF") #adding new attribute
-
-    def __secret(self):
+# child class
+class ElectricGuiter(Guiter) :  # inheriting everything from Guiter class
+    # def __init__(self) :  # changing something from parent class
+    # super().__init__()  # allowing the subclass to override methods or attributes of parent class and provide its
+    # own implementation.
+    # self.n_string = 8
+    # self.color = ("#000000", "#FFFFFF")  # adding new attribute
+    def __secret(self) :
         print("This Guiter actually cost me $", self._Guiter__cost, "Only!")
 
-    def playlowder(self): # adding something to it
-        print("Pa para parappa pappa pa pa pa".upper())
+    def playlowder(self) :  # adding something to it
+        print("I am from Child class".upper())
 
-child_class = ElectricGuiter()
 
+child_class = ElectricGuiter(8)
 print("child class : ", child_class.n_string)
-print("Parent Class : ", Guiter().n_string)
+# print("Parent Class : ", Guiter.n_string)
 
-child_class._ElectricGuiter__secret()
-
-
-"""class Animal :
-    def __init__(self, name) :
-        self.name = name
-
-    def speak(self) :
-        print(f"{self.name} says hello")
-
-class Dog(Animal) :
-    def __init__(self, name, breed) :
-        super().__init__(name)
-        self.breed = breed
-
-    def speak(self) :
-        super().speak()
-        print("Woof")
+child_class._ElectricGuiter__secret()"""
 
 
-my_dog = Dog("Fido", "Labrador")
-my_dog.speak()
-"""
+class Guitar:
+    def __init__(self):
+        self.n_string = 8
+        self.play()
+        self.__cost = 50
+    def play(self):
+        print("I'm in parent class")
 
+class baseGuiter(Guitar):
+    pass # adding pass to copy & paste exact same from Guitar parent class
+
+class ElectricGuitar(Guitar):
+    def __init__(self):
+        super().__init__()
+        self.n_string = 10
+        self.color = ("#FFFFFF", "#000000")
+        #self.__cost = 50 # __cost -> __ is used to make it private
+
+    def play_louder(self):
+        print("I'm in Child class")
+
+    def __secret(self): # private method
+        print("This is Private Guitar, this cost me around $", self._Guitar__cost, "Only!")
+
+my_guitar = ElectricGuitar()
+#print("Guitar's secreat price : ", my_guitar._Guitar__cost) # _ElectricGuitar__cost or _Guitar__cost ->  <_classname> is used print private object
+my_guitar._ElectricGuitar__secret()
