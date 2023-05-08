37965559bb43e3e437505c655671becc88c466cb
Aishwarya <aishwarya99roy@gmail.com>
2023-05-09 01:58:22 +0530

    Update

--- 2023 - 05 - May/Class_Object/main.py
+++ 2023 - 05 - May/Class_Object/main.py
@@ -57,54 +57,23 @@ my_Guiter = guiter()"""
 
 # inheritance
 
-# parent class
-"""class Guiter :
-    def __init__(self, n_string) :
-        self.n_string = n_string  # attribute
-        self.play()
+#parent class
+"""class Guitar:
+    def __init__(self, n_string=6):
+        self.n_string = n_string
         self.__cost = 50
-
-    def play(self) :  # self parameter inside the new function makes it a method
-        print("I from Parent class ")
-
-
-# child class
-class ElectricGuiter(Guiter) :  # inheriting everything from Guiter class
-    # def __init__(self) :  # changing something from parent class
-    # super().__init__()  # allowing the subclass to override methods or attributes of parent class and provide its
-    # own implementation.
-    # self.n_string = 8
-    # self.color = ("#000000", "#FFFFFF")  # adding new attribute
-    def __secret(self) :
-        print("This Guiter actually cost me $", self._Guiter__cost, "Only!")
-
-    def playlowder(self) :  # adding something to it
-        print("I am from Child class".upper())
-
-
-child_class = ElectricGuiter(8)
-print("child class : ", child_class.n_string)
-# print("Parent Class : ", Guiter.n_string)
-
-child_class._ElectricGuiter__secret()"""
-
-
-class Guitar:
-    def __init__(self):
-        self.n_string = 8
-        self.play()
-        self.__cost = 50
-    def play(self):
+    def play(self): # self parameter inside the new function makes it a "Method"
         print("I'm in parent class")
 
-class baseGuiter(Guitar):
+#child class
+class bassGuiter(Guitar):
     pass # adding pass to copy & paste exact same from Guitar parent class
 
-class ElectricGuitar(Guitar):
+#child class
+class ElectricGuitar(Guitar): # inheriting everything from Guitar class
     def __init__(self):
-        super().__init__()
-        self.n_string = 10
-        self.color = ("#FFFFFF", "#000000")
+        super().__init__(n_string = 10) # supar() -> allowing the subclass to override methods or attributes of parent class and provide its own implementation.
+        self.color = ("#FFFFFF", "#000000") # adding new attribute in child class
         #self.__cost = 50 # __cost -> __ is used to make it private
 
     def play_louder(self):
@@ -116,3 +85,5 @@ class ElectricGuitar(Guitar):
 my_guitar = ElectricGuitar()
 #print("Guitar's secreat price : ", my_guitar._Guitar__cost) # _ElectricGuitar__cost or _Guitar__cost ->  <_classname> is used print private object
 my_guitar._ElectricGuitar__secret()
+print("In base guitar : ", bassGuiter(4).n_string)
+print("In Electric guitar : ",my_guitar.n_string)"""
