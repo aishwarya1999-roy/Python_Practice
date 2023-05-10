5e1a5be9edc7a3c9aa339f776a46a3b3941aafa4
Aishwarya <aishwarya99roy@gmail.com>
2023-05-10 13:43:05 +0530

    Update

---
+++ 2023 - 05 - May/10-May/main.py
@@ -0,0 +1,18 @@
+# __name--
+"""import pandas as pd
+
+df= pd.DataFrame([1, 3])
+print("This is module")
+print(__name__)"""
+
+# __name__ == "__main__"
+
+"""def callme() :
+    print("Helloo!!")
+if __name__ == "__main__" :  # if we add this , then the callme() function will only execute when this module is a top
+    # level module, even if this program/module imported into some other program, then also the callme() module will 
+    # not work. Top Level Module - The module (Any function, class) present in the current program, not imported from
+    # some others program 
+    callme()
+"""
+
