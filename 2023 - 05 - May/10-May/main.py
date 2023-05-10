bb369b956b536e9fa2819ec3dfe3dc8bed935b42
Aishwarya <aishwarya99roy@gmail.com>
2023-05-11 00:51:34 +0530

    Update

--- 2023 - 05 - May/10-May/main.py
+++ 2023 - 05 - May/10-May/main.py
@@ -29,7 +29,9 @@ print(bits)"""
 # String Manipulation
 
 my_string = "HelloMyNameIsAish"
-my_string = "".join([i if i.islower() else " " + i for i in my_string])
+my_string = "".join(
+    [i if i.islower() else " " + i.lower() if i in ["N", "I"] else " " + i for i in my_string]
+    )[1:]
 print(my_string)
 
 
