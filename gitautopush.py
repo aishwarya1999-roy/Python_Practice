db8d94237c08597a2d3a24260ce4f511c8695369
Aishwarya <aishwarya99roy@gmail.com>
2023-05-10 13:50:54 +0530

    Update

---
+++ gitautopush.py
@@ -0,0 +1,11 @@
+import subprocess
+import time
+
+def git_auto_commit(comment):
+    subprocess.run(["git", "add", "."])
+    subprocess.run(["git", "commit", "-m", comment])
+    subprocess.run(["git", "push"])
+
+while True:
+    git_auto_commit("Update")
+    time.sleep(10)  # Sleep for 10 seconds
