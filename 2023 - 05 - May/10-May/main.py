1b1635439740e5029ffa0cafafba11b8f47da3ee
Aishwarya <aishwarya99roy@gmail.com>
2023-05-15 05:08:35 +0530

    Auto-commit

--- 2023 - 05 - May/10-May/main.py
+++ 2023 - 05 - May/10-May/main.py
@@ -74,7 +74,7 @@ def generate_random_word(original_word) :
 
 
 # Example usage
-original_word = "123"
+original_word = "1aA@"
 random_word = generate_random_word(original_word)
 print(f"Random Word: {random_word}")
 
