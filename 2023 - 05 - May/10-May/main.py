3687935119d085267df66e6df990342530f2f8d3
Aishwarya <aishwarya99roy@gmail.com>
2023-05-15 22:41:14 +0530

    Auto-commit

--- 2023 - 05 - May/10-May/main.py
+++ 2023 - 05 - May/10-May/main.py
@@ -56,9 +56,9 @@ print(my_dict)
 """
 
 my_dict = {
-    "Spider" : "123",
-    "Bat" : "22",
-    "Wonder Wo" : "54"
+    "Spider": "123",
+    "Bat": "22",
+    "Wonder Wo": "54"
 }
 my_dict = {key + "man" : val for (key, val) in my_dict.items()}
 print(my_dict)
