cf4d3be231c6c0d34e1d93875a55837c3841aeeb
Aishwarya <aishwarya99roy@gmail.com>
2023-05-16 01:00:12 +0530

    Auto-commit

--- 2023 - 05 - May/10-May/main.py
+++ 2023 - 05 - May/10-May/main.py
@@ -84,8 +84,6 @@ import string
 keys = ["id", "username", "password"]
 users = ["Queen", "Blacy"]
 
-data = [{key: {i if key == "id" else users[i] if key == "username" else None} for key in keys} for i in range(len(users))]
-#print(data)
+data = [{key: {i if key == "id" else users[i] if key == "username" else ''.join(random.choices(string.printable, k = 8))} for key in keys} for i in range(len(users))]
+print(data)
 
-password = ''.join(random.choices(string.printable, k = 8))
-print(password)
