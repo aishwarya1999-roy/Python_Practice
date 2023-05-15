3b5b749b49bcd3793be28126b01f121d354e729e
Aishwarya <aishwarya99roy@gmail.com>
2023-05-15 23:00:52 +0530

    Auto-commit

--- 2023 - 05 - May/10-May/main.py
+++ 2023 - 05 - May/10-May/main.py
@@ -68,5 +68,5 @@ print(my_dict)
 # if we don't know the data of the dict.
 import random
 bases = ["A", "T", "C", "G"]
-stand1 = random.choices(bases, k =5)
+stand1 = random.choices(bases, k =10)
 print(stand1)
