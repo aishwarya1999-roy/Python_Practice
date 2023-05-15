b383dc6fe9108345b9537c9d3946c69477312cc1
Aishwarya <aishwarya99roy@gmail.com>
2023-05-16 00:55:47 +0530

    Auto-commit

--- 2023 - 05 - May/10-May/main.py
+++ 2023 - 05 - May/10-May/main.py
@@ -87,4 +87,4 @@ users = ["Queen", "Blacy"]
 data = [{key: {i if key == "id" else users[i] if key == "username" else None} for key in keys} for i in range(len(users))]
 print(data)
 
-random
+random.choice(string.printable, k = 8)
