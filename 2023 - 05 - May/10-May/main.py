8d1ea9ba6373990860f7248b4a3a2d678f425a0a
Aishwarya <aishwarya99roy@gmail.com>
2023-05-16 01:14:49 +0530

    Auto-commit

--- 2023 - 05 - May/10-May/main.py
+++ 2023 - 05 - May/10-May/main.py
@@ -79,14 +79,14 @@ print(dna)"""
 
 
 # list of dictionaries
-import random
+"""import random
 import string
 keys = ["id", "username", "password"]
 users = ["Queen", "Blacy", "Lady", "Vampire"]
 
 data = [{key: {i if key == "id" else users[i] if key == "username" else ''.join(random.choices(string.printable, k = 8))} for key in keys} for i in range(len(users))]
 print(data)
-
+"""
 
 
 
