cb7689c7f5cc928d473f448e9e2b0ade0fd8647a
Aishwarya <aishwarya99roy@gmail.com>
2023-05-15 22:53:32 +0530

    Auto-commit

--- 2023 - 05 - May/10-May/main.py
+++ 2023 - 05 - May/10-May/main.py
@@ -60,7 +60,7 @@ my_dict = {
     "Bat": "philanthropist",
     "Wonder Wo": "nurse"
 }
-my_dict = {(key + "man" if key!= "Spider" else "Superman") : (val if val!= "photo") for (key, val) in my_dict.items()}
+my_dict = {(key + "man" if key!= "Spider" else "Superman") : val for (key, val) in my_dict.items()}
 print(my_dict)
 
 
