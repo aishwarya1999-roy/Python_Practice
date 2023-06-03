fdf5c81782ef6c10f88be373b06a2c409d8978ba
Aishwarya <aishwarya99roy@gmail.com>
2023-06-04 02:54:54 +0530

    Auto-commit

--- 2023 - 06 - June/Roman_To_Interger/main.py
+++ 2023 - 06 - June/Roman_To_Interger/main.py
@@ -13,7 +13,7 @@ dict_roman = {"I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 
 S = "IV"
 L = 0
 for i in range(len(S)-1):
-    print(S[i])
+    print(dict_roman[S[i]])
 print(L)
 
 
