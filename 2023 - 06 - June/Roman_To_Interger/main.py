3f830dcb5539dd760151fd05af7b478431501504
Aishwarya <aishwarya99roy@gmail.com>
2023-06-04 01:21:22 +0530

    Auto-commit

--- 2023 - 06 - June/Roman_To_Interger/main.py
+++ 2023 - 06 - June/Roman_To_Interger/main.py
@@ -18,7 +18,7 @@ print("Individual alphabets:", sliced_alphabets)
 
 def romanToInt(x: str) :
     dict_roman = {"I" : "1", "V" : "5", "X" : "10", "L" : "50", "C" : "100", "D" : "500", "M" : "1000"}
-    d = int(''.join([val for (key, val) in dict_roman.items() if key == x]))
+    d = int(''.join([val if key == x for (key, val) in dict_roman.items()]))
     print(d)
 
 
