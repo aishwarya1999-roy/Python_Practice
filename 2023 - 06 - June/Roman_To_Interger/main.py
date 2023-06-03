625394abe008288337c488499f9df682ce246125
Aishwarya <aishwarya99roy@gmail.com>
2023-06-04 00:29:32 +0530

    Auto-commit

--- 2023 - 06 - June/Roman_To_Interger/main.py
+++ 2023 - 06 - June/Roman_To_Interger/main.py
@@ -1,4 +1,4 @@
-def romanToInt() :
+def romanToInt(self, s: str) -> int :
     dict_roman = {"I" : "1", "V" : "5", "X" : "10", "L" : "50", "C" : "100", "D" : "500", "M" : "1000"}
 
     print(dict_roman)
