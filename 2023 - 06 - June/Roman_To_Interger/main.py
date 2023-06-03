2da89925fb9dfb99d6fb4fd477ffef39e1dd9e0c
Aishwarya <aishwarya99roy@gmail.com>
2023-06-04 01:03:49 +0530

    Auto-commit

--- 2023 - 06 - June/Roman_To_Interger/main.py
+++ 2023 - 06 - June/Roman_To_Interger/main.py
@@ -9,11 +9,13 @@
 
 romanToInt('II')
 """
-# Get a string input from the user
-user_input = input("Enter a string: ")
 
-# Slice the string into individual alphabets
+"""user_input = input("Enter a string: ")
 sliced_alphabets = [x for x in user_input]
-
-# Print the sliced alphabets
 print("Individual alphabets:", sliced_alphabets)
+"""
+
+def romanToInt(x: str) :
+    dict_roman = {"I" : "1", "V" : "5", "X" : "10", "L" : "50", "C" : "100", "D" : "500", "M" : "1000"}
+    d = {val for (key,val) in dict.items() if key == x}
+    print(d)
