e57747dca8848e8d67b91a23972e548ccd9fd162
Aishwarya <aishwarya99roy@gmail.com>
2023-06-04 00:28:16 +0530

    Auto-commit

--- 2023 - 06 - June/Roman_To_Interger/main.py
+++ 2023 - 06 - June/Roman_To_Interger/main.py
@@ -1,5 +1,5 @@
 class Solution :
-    def romanToInt(self, s: str) -> int :
+    def romanToInt(self) :
         dict_roman = {"I" : "1", "V" : "5", "X" : "10", "L" : "50", "C" : "100", "D" : "500", "M" : "1000"}
 
         print(dict_roman)
