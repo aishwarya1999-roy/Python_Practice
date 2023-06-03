bca72637528f860940d8942c6f26f51320574434
Aishwarya <aishwarya99roy@gmail.com>
2023-06-04 02:30:40 +0530

    Auto-commit

--- 2023 - 06 - June/Roman_To_Interger/main.py
+++ 2023 - 06 - June/Roman_To_Interger/main.py
@@ -11,7 +11,7 @@ S.romanToInt('I')"""
 
 dict_roman = {"I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000}
 minus = {"IV":4, "IX":9, "XL":40, "XC":90, "CD":400, "CM":900}
-S = "XIV"
+S = "IV"
 L = 0
 sliced = [x for x in S]
 for key, val in dict_roman.items():
