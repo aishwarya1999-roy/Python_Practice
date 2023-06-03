930ec7b17a43f2bbb537eba9665ad9dd534757bc
Aishwarya <aishwarya99roy@gmail.com>
2023-06-04 01:59:25 +0530

    Auto-commit

--- 2023 - 06 - June/Roman_To_Interger/main.py
+++ 2023 - 06 - June/Roman_To_Interger/main.py
@@ -8,22 +8,23 @@
 
 romanToInt('II')"""
 
-dict_roman = {"I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000}
+"""dict_roman = {"I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000}
 user_input = input("Enter a string: ")
 for key, val in dict_roman.items():
     sliced_alphabets = [x for x in user_input]
     print("Individual alphabets:", sliced_alphabets)
     sum(sliced_alphabets)
+"""
 
 
 
 
-"""def romanToInt(x: str) :
+def romanToInt(x: str) :
     dict_roman = {"I" : "1", "V" : "5", "X" : "10", "L" : "50", "C" : "100", "D" : "500", "M" : "1000"}
     #d = int(''.join([val if key == x else None for (key, val) in dict_roman.items()]))
-    d = [val if key == x else None for (key, val) in dict_roman.items()]
+    d = [val if x == key else None for (key, val) in dict_roman.items()]
     print(d)
 
 
 romanToInt('I')
-"""
+
