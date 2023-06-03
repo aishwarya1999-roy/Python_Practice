2676dd8c5e9bf03dab3791156f81bba9cef627e9
Aishwarya <aishwarya99roy@gmail.com>
2023-06-04 01:45:35 +0530

    Auto-commit

--- 2023 - 06 - June/Roman_To_Interger/main.py
+++ 2023 - 06 - June/Roman_To_Interger/main.py
@@ -1,4 +1,4 @@
-def romanToInt(x : str) :
+"""def romanToInt(x : str) :
         dict_roman = {"I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000}
 
         for key, val in dict_roman.items():
@@ -6,7 +6,18 @@ def romanToInt(x : str) :
                 print(val)
             else:
 
-romanToInt('II')
+
+
+
+romanToInt('II')"""
+
+
+
+user_input = input("Enter a string: ")
+sliced_alphabets = [x for x in user_input]
+print("Individual alphabets:", sliced_alphabets)
+
+
 
 
 """def romanToInt(x: str) :
