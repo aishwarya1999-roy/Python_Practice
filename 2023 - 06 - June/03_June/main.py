31c4c29933281f2b3f778333548c3d3c3294a287
Aishwarya <aishwarya99roy@gmail.com>
2023-06-03 21:44:04 +0530

    Auto-commit

--- 2023 - 06 - June/03_June/main.py
+++ 2023 - 06 - June/03_June/main.py
@@ -6,6 +6,9 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+    def insert_at_beginnin(self, data):
+        node  = Node(data, self.head)
+        self.head = node
 
     def print(self):
         if self.head is None:
@@ -20,5 +23,6 @@ class LinkedList:
 
 if __name__ == '__main__':
     ll = LinkedList()
-    ll.insert
+    ll.insert_at_beginnin(5)
+    ll.insert_at_beginnin(10)
     ll.print()
