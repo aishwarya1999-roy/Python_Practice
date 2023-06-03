a9800c59eb7d91bda345eeafc68677e95362de05
Aishwarya <aishwarya99roy@gmail.com>
2023-06-04 03:30:34 +0530

    Auto-commit

--- 2023 - 06 - June/Roman_To_Interger/main.py
+++ 2023 - 06 - June/Roman_To_Interger/main.py
@@ -38,5 +38,5 @@ romanToInt('I')"""
     print(d)
 romanToInt('I')"""
 dict_roman = {"I" : "1", "V" : "5", "X" : "10", "L" : "50", "C" : "100", "D" : "500", "M" : "1000"}
-s = "II"
+s = "I"
 print(dict_roman[s])
