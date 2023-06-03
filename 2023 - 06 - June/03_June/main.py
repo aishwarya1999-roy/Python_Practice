ab1ecf6123559816db41937fb3e61f1b9528aa47
Aishwarya <aishwarya99roy@gmail.com>
2023-06-03 21:38:21 +0530

    Auto-commit

--- 2023 - 06 - June/03_June/main.py
+++ 2023 - 06 - June/03_June/main.py
@@ -6,3 +6,8 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+
+    def print(self):
+        if self.head is None:
+            print("Linked list is empty")
+            return
