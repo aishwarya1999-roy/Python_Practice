df0e120b05e2904fff4042497c1a132f06199f32
Aishwarya <aishwarya99roy@gmail.com>
2023-06-04 00:29:07 +0530

    Auto-commit

--- 2023 - 06 - June/Roman_To_Interger/main.py
+++ 2023 - 06 - June/Roman_To_Interger/main.py
@@ -1,9 +1,8 @@
-class Solution :
-    def romanToInt(self) :
-        dict_roman = {"I" : "1", "V" : "5", "X" : "10", "L" : "50", "C" : "100", "D" : "500", "M" : "1000"}
+def romanToInt(self) :
+    dict_roman = {"I" : "1", "V" : "5", "X" : "10", "L" : "50", "C" : "100", "D" : "500", "M" : "1000"}
 
-        print(dict_roman)
+    print(dict_roman)
 
 
-if __name__ == "__main__":
-    Solution
+if __name__ == "__main__" :
+    romanToInt()
