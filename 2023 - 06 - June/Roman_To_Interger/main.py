b4239e5316c0cf21f880a3e51a5ebe532b108612
Aishwarya <aishwarya99roy@gmail.com>
2023-06-04 04:02:11 +0530

    Auto-commit

--- 2023 - 06 - June/Roman_To_Interger/main.py
+++ 2023 - 06 - June/Roman_To_Interger/main.py
@@ -1,14 +1,3 @@
-"""def romanToInt(S) :
-    dict_roman = {"I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000}
-    L = 0
-    for i in range(len(S)-1):
-        if dict_roman[S[i]] < dict_roman[S[i+1]]:
-            L = L - dict_roman[S[i]]
-        else:
-            L = L + dict_roman[S[i]]
-    return L + dict_roman[S[-1]]
-print(romanToInt("I"))"""
-
 def romanToInt(S) :
     dict_roman = {"I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000}
     L = 0
@@ -20,11 +9,3 @@ def romanToInt(S) :
     return L + dict_roman[S[-1]]
     
 print(romanToInt("XIX"))
-
-
-"""def romanToInt(x: str) :
-    dict_roman = {"I" : "1", "V" : "5", "X" : "10", "L" : "50", "C" : "100", "D" : "500", "M" : "1000"}
-    #d = int(''.join([val if key == x else None for (key, val) in dict_roman.items()]))
-    d = [val if x == key else None for (key, val) in dict_roman.items()]
-    print(d)
-romanToInt('I')"""
