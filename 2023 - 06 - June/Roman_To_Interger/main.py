c4d6a5683661c8565f95727b1c10a3e3f2961477
Aishwarya <aishwarya99roy@gmail.com>
2023-06-04 02:53:00 +0530

    Auto-commit

--- 2023 - 06 - June/Roman_To_Interger/main.py
+++ 2023 - 06 - June/Roman_To_Interger/main.py
@@ -12,7 +12,8 @@ S.romanToInt('I')"""
 dict_roman = {"I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000}
 S = "IV"
 L = 0
-for i in 
+for i in range(len(S)-1):
+    
 print(L)
 
 
