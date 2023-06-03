80bff2fa2f1a2e3b7afde7d6b9ba77d87115597f
Aishwarya <aishwarya99roy@gmail.com>
2023-06-04 02:00:26 +0530

    Auto-commit

--- 2023 - 06 - June/Roman_To_Interger/main.py
+++ 2023 - 06 - June/Roman_To_Interger/main.py
@@ -1,12 +1,13 @@
-"""def romanToInt(x : str) :
-        dict_roman = {"I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000}
-
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        dict_roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         for key, val in dict_roman.items():
-            if key == x:
-                print(val)
-            else:
+            if s == key:
+                print(int(val))
 
-romanToInt('II')"""
+S = Solution()
+S.romanToInt('I')
+
 
 """dict_roman = {"I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000}
 user_input = input("Enter a string: ")
@@ -19,12 +20,12 @@ for key, val in dict_roman.items():
 
 
 
-def romanToInt(x: str) :
+"""def romanToInt(x: str) :
     dict_roman = {"I" : "1", "V" : "5", "X" : "10", "L" : "50", "C" : "100", "D" : "500", "M" : "1000"}
     #d = int(''.join([val if key == x else None for (key, val) in dict_roman.items()]))
     d = [val if x == key else None for (key, val) in dict_roman.items()]
     print(d)
 
 
-romanToInt('I')
+romanToInt('I')"""
 
