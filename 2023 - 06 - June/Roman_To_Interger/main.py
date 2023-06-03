436cd3a0f507d0017102f02bdf056edb0d24b8b0
Aishwarya <aishwarya99roy@gmail.com>
2023-06-04 02:57:57 +0530

    Auto-commit

--- 2023 - 06 - June/Roman_To_Interger/main.py
+++ 2023 - 06 - June/Roman_To_Interger/main.py
@@ -10,10 +10,12 @@ S.romanToInt('I')"""
 
 
 dict_roman = {"I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000}
-S = "VI"
+S = "IV"
 L = 0
 for i in range(len(S)-1):
-    print(dict_roman[S[i+1]])
+    if dict_roman[S[i]] < dict_roman[S[i+1]]:
+        L = 
+
 #print(L)
 
 
