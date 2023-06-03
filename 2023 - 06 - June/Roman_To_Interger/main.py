fb4003349b39c1929550b010f3a85cc9164f71f5
Aishwarya <aishwarya99roy@gmail.com>
2023-06-04 03:18:52 +0530

    Auto-commit

--- 2023 - 06 - June/Roman_To_Interger/main.py
+++ 2023 - 06 - June/Roman_To_Interger/main.py
@@ -1,15 +1,15 @@
-"""class Solution:
-    def romanToInt(self, s: str) -> int:
-        dict_roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-        for key, val in dict_roman.items():
-            if s == key:
-                print(int(val))
+"""def romanToInt(S) :
+    dict_roman = {"I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000}
+    L = 0
+    for i in range(len(S)-1):
+        if dict_roman[S[i]] < dict_roman[S[i+1]]:
+            L = L - dict_roman[S[i]]
+        else:
+            L = L + dict_roman[S[i]]
+    return L + dict_roman[S[-1]]
+print(romanToInt("I"))"""
 
-S = Solution()
-S.romanToInt('I')"""
-
-
-def romanToInt(S) :
+"""def romanToInt(S) :
     dict_roman = {"I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000}
     L = 0
     for i in range(len(S)-1):
@@ -19,8 +19,7 @@ def romanToInt(S) :
             L = L + dict_roman[S[i]]
     return L + dict_roman[S[-1]]
 print(romanToInt("I"))
-
-
+"""
 
 
 """def romanToInt(x: str) :
@@ -32,3 +31,11 @@ print(romanToInt("I"))
 
 romanToInt('I')"""
 
+def romanToInt(x: str) :
+    dict_roman = {"I" : "1", "V" : "5", "X" : "10", "L" : "50", "C" : "100", "D" : "500", "M" : "1000"}
+    #d = int(''.join([val if key == x else None for (key, val) in dict_roman.items()]))
+    d = [dict_roman[x[i]] if x == i else None for i in range(len(x)-1)]
+    print(d)
+
+
+romanToInt('I')
