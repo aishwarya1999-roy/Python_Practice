e6d3a58825261075da1d5f94042456873b493016
Aishwarya <aishwarya99roy@gmail.com>
2023-06-04 01:28:37 +0530

    Auto-commit

--- 2023 - 06 - June/Roman_To_Interger/main.py
+++ 2023 - 06 - June/Roman_To_Interger/main.py
@@ -1,9 +1,9 @@
 def romanToInt(x : str) :
-        dict_roman = {"I" : "1", "V" : "5", "X" : "10", "L" : "50", "C" : "100", "D" : "500", "M" : "1000"}
+        dict_roman = {"I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000}
 
         for key, val in dict_roman.items():
             if key == x:
-                print(int(val))
+                print(val)
 
 
 
