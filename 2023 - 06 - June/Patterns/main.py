d5281cab9f98a22bde99fc8548f75d727d2dcafa
Aishwarya <aishwarya99roy@gmail.com>
2023-06-05 01:20:00 +0530

    Auto-commit

--- 2023 - 06 - June/Patterns/main.py
+++ 2023 - 06 - June/Patterns/main.py
@@ -30,10 +30,12 @@ for i in range(5, 0, -1):
 for i in range(1, 6):
     c.append(str(i))
     print(int(''.join(c)))"""
-for i in range(1, 6):
+#or
+
+"""for i in range(1, 6):
     for j in range(1, i+1):
         print(j, end = ' ')
-    print()
+    print()"""
 
 
 #5 4 3 2 1
