89c4bc6abd35b2b3616205938c20acb14c1a5004
Aishwarya <aishwarya99roy@gmail.com>
2023-06-04 22:35:47 +0530

    Auto-commit

--- 2023 - 06 - June/Patterns/main.py
+++ 2023 - 06 - June/Patterns/main.py
@@ -4,7 +4,12 @@
 #****
 #*****
 
-c = []
+"""c = []
 for i in range(0,5):
     c.append('*')
-    print(''.join(c))
+    print(''.join(c))"""
+
+c = []
+for i in range(5, 0, -1):
+    c.append('*' * i)
+    print(c)
