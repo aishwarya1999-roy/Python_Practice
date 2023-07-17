1661d24c0c9af690e9ae89217c1f28c635960d88
Aishwarya Roy <aishwarya99roy@gmail.com>
2023-07-18 05:02:53 +0530

    Auto-commit

---
+++ 2023 - 07 - July/18 - July/main.py
@@ -0,0 +1,11 @@
+# ARRAY
+# Two Sum: Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+array = [1,2,3,5,8]
+
+inp = int(input("Set the target : "))
+f = 0
+for i in range(len(array)):
+    print(i)
+
+
