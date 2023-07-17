aa409f6f464c8d4e17cb09afea2342f08f94a6a3
Aishwarya Roy <aishwarya99roy@gmail.com>
2023-07-18 05:11:05 +0530

    Auto-commit

--- 2023 - 07 - July/18 - July/main.py
+++ 2023 - 07 - July/18 - July/main.py
@@ -2,9 +2,7 @@
 # Two Sum: Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
 array = [1,2,3,5,8]
-
 inp = 3
-f = 0
 def twosum():
     for i in range(len(array)):
         for j in range(i+1, len(array)):
