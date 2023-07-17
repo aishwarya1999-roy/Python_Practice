ae603d2217de01304242abf1c0aa1a58086b6cd5
Aishwarya Roy <aishwarya99roy@gmail.com>
2023-07-18 05:25:59 +0530

    Auto-commit

--- 2023 - 07 - July/18 - July/main.py
+++ 2023 - 07 - July/18 - July/main.py
@@ -2,6 +2,7 @@
 # Two Sum: Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
 array = [8, 2, 7, 5, 1]
+f  = 0
 inp = int(input("Enter the target : "))
 arr = sorted(array)[::-1]
 def twosum():
@@ -10,5 +11,6 @@ def twosum():
             if array[i] + array[j] == inp:
                 return [i, j]
             else:
-                print("Error")
+                f = 1
+                return f
 print(twosum())
