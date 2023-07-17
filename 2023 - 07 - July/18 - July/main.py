5a03ca949d8baa3bf03e3ae9883e721027433231
Aishwarya Roy <aishwarya99roy@gmail.com>
2023-07-18 05:17:41 +0530

    Auto-commit

--- 2023 - 07 - July/18 - July/main.py
+++ 2023 - 07 - July/18 - July/main.py
@@ -1,7 +1,7 @@
 # ARRAY
 # Two Sum: Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
-array = [1, 2, 3, 5, 8]
+array = [5, 2, 7, 5, 8]
 inp = int(input("Enter the target : "))
 arr = sorted(array)
 print(arr)
