53e358f79883b32486ee81c788d07d0bb74f404f
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-01 10:07:54 +0530

    Auto-commit

--- 2024 - 07 -  July/bit_manupulation.py
+++ 2024 - 07 -  July/bit_manupulation.py
@@ -1,4 +1,4 @@
-def decimal_to_binary(x):
+"""def decimal_to_binary(x):
     res = ""
     while x > 0:
         if x%2 == 1:
@@ -8,19 +8,24 @@ def decimal_to_binary(x):
         x//=2
     return res[::-1]
 x = 15
-print(decimal_to_binary(x))
+print(decimal_to_binary(x))"""
 
 
 
 
-def binary_to_decimal(binary_str):
+"""def binary_to_decimal(binary_str):
     decimal_value = 0
     for i, digit in enumerate (reversed (binary_str)):
         if digit == '1':
             decimal_value += 2 ** i
     return decimal_value
 binary_string = "101"
-print(binary_to_decimal(binary_string))
+print(binary_to_decimal(binary_string))"""
+
+a = 5
+b = 6
+
+print(a,b)
 
 
 
