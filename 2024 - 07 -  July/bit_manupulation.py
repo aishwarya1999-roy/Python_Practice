572b86148907efdd44854878321d1c20593fc009
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-02 07:57:05 +0530

    Auto-commit

--- 2024 - 07 -  July/bit_manupulation.py
+++ 2024 - 07 -  July/bit_manupulation.py
@@ -55,7 +55,7 @@ print(n|(1<<i))"""
 
 # Three Consecutive Odds
 
-arr = [1, 2, 3, 4, 3, 9, 7]
+arr = [1, 2, 3, 4, 3, 9, 7, 4, 5,7,9]
 n = len(arr)
 
 for i in range(n - 2):  # Adjust the loop to stop at n-2
