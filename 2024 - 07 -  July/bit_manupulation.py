bd3565ccfddc85252632ed05aaa26af717b4ecb2
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-02 08:20:45 +0530

    Auto-commit

--- 2024 - 07 -  July/bit_manupulation.py
+++ 2024 - 07 -  July/bit_manupulation.py
@@ -54,10 +54,26 @@ i = 2
 print(n|(1<<i))"""
 
 # Three Consecutive Odds
-
-arr = [1, 2, 3, 4, 3, 9, 7, 4, 5,7,9]
+#approch 1
+"""arr = [1, 2, 3, 4, 3, 9, 7, 4, 5,7,9]
 n = len(arr)
 
 for i in range(n - 2):  # Adjust the loop to stop at n-2
     if arr[i] % 2 != 0 and arr[i + 1] % 2 != 0 and arr[i + 2] % 2 != 0:
-        print(arr[i], arr[i + 1], arr[i + 2])
+        print(arr[i], arr[i + 1], arr[i + 2])"""
+
+#approch 2
+def three_consecutive_odds(arr):
+    count = 0
+    for num in arr:
+        if num % 2 != 0:
+            count += 1
+            if count == 3:
+                return True
+        else:
+            count = 0
+    return False
+
+# Example usage:
+arr = [2, 6, 4, 1, 3, 5]
+print(three_consecutive_odds(arr))  # Output: True
