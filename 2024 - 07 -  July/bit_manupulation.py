a9e576029753981e355be38d5b0d9ba6f92e71cc
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-05 01:01:20 +0530

    Auto-commit

--- 2024 - 07 -  July/bit_manupulation.py
+++ 2024 - 07 -  July/bit_manupulation.py
@@ -1,28 +1,3 @@
-# decimal_to_binary
-"""def decimal_to_binary(x):
-    res = ""
-    while x > 0:
-        if x%2 == 1:
-            res+="1"
-        else:
-            res+="0"
-        x//=2
-    return res[::-1]
-x = 15
-print(decimal_to_binary(x))"""
-
-
-# binary_to_decimal
-
-"""def binary_to_decimal(binary_str):
-    decimal_value = 0
-    for i, digit in enumerate (reversed (binary_str)):
-        if digit == '1':
-            decimal_value += 2 ** i
-    return decimal_value
-binary_string = "101"
-print(binary_to_decimal(binary_string))"""
-
 # Swap two numbers without using third variable
 """a = 5
 b = 6
@@ -55,8 +30,8 @@ print(n|(1<<i))"""
 
 #clear the set in ith bit
 """n = 9
-i = 2"""
-print(n& ~(1<<i))
+i = 2
+print(n& ~(1<<i))"""
 
 #toggle the ith bit
 
