d0a10a665496b7d5920e8237cb4bd1490a5fab69
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-05 01:40:14 +0530

    Auto-commit

--- 2024 - 07 -  July/decimal-to-binary-vice-versa.py
+++ 2024 - 07 -  July/decimal-to-binary-vice-versa.py
@@ -1,5 +1,5 @@
 # decimal_to_binary
-"""def decimal_to_binary(x):
+def decimal_to_binary(x):
     res = ""
     while x > 0:
         if x%2 == 1:
@@ -9,16 +9,16 @@
         x//=2
     return res[::-1]
 x = 15
-print(decimal_to_binary(x))"""
+print(decimal_to_binary(x))
 
 
 # binary_to_decimal
 
-"""def binary_to_decimal(binary_str):
+def binary_to_decimal(binary_str):
     decimal_value = 0
     for i, digit in enumerate (reversed (binary_str)):
         if digit == '1':
             decimal_value += 2 ** i
     return decimal_value
 binary_string = "101"
-print(binary_to_decimal(binary_string))"""
+print(binary_to_decimal(binary_string))
