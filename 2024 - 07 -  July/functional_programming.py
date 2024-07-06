8167066e55df259e59ad55494e12d20a17e6f822
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-06 14:02:27 +0530

    Auto-commit

--- 2024 - 07 -  July/functional_programming.py
+++ 2024 - 07 -  July/functional_programming.py
@@ -3,8 +3,16 @@
 print(add(5,6))"""
 
 
-add2= lambda x,y : x+y
+"""add2= lambda x,y : x+y
 print(add2(5,4))
 
 
-print((lambda x,y : x+y)(6,4))
+print((lambda x,y : x+y)(6,4))"""
+
+def map_p(my_func, my_iter):
+    res =[]
+    for i in my_iter:
+        new_item = my_func(i)
+        res.append(new_item)
+    return(res)
+nums = [3,4,5,6,7]
