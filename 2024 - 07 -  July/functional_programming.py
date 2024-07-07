688a9d5212eeb106054345c24844ec27087133ae
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-07 11:00:44 +0530

    Auto-commit

--- 2024 - 07 -  July/functional_programming.py
+++ 2024 - 07 -  July/functional_programming.py
@@ -2,12 +2,15 @@
     return x+y
 print(add(5,6))"""
 
+#Lambda function
 
 """add2= lambda x,y : x+y
 print(add2(5,4))
 
 
 print((lambda x,y : x+y)(6,4))"""
+
+# mannual map function
 
 """def map_p(my_func, my_iter):
     res =[]
@@ -20,20 +23,21 @@ nums = [3,4,5,6,7]
 squared = map_p(lambda x: x**2, nums)
 print(squared)
 """
-
+#map function
 """def map_p(my_iter):
     return my_iter**2
 
 nums = [3,4,5,6,7]
 
-y = [map_p(num) for num in nums]
+y = [map_p(num) for num in nums] #list comprehension
 
-z=list(map(map_p, nums))
+z=list(map(map_p, nums))#map function
 
 print(y)
 print(z)
 """
 
+# filter function
 
 """nums = [5,9,14,21,566]
 
@@ -42,5 +46,14 @@ def even(x):
 
 print(list(filter(even,nums)))"""
 
+#reduce function    
+"""from functools import reduce
+arr1 = [1,1,5,4,3,3,5,6,7,2,7,6,2]"""
 
-arr1 = [1,1,5,4,3,3,5,6,7,2,7,6,2]
+
+from functools import reduce
+
+arr1 = [3,2,1,4,5]
+print(reduce(lambda x,y : x+y, arr1))
+print(reduce(lambda x,y : x if x>y else y, arr1))
+print(reduce(lambda x,y : x if x<y else y, arr1))
