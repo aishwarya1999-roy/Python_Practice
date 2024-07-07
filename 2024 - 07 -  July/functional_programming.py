2bc491ca7c97d78ca107bcde5b5046c98b06fb13
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-07 12:14:02 +0530

    Auto-commit

--- 2024 - 07 -  July/functional_programming.py
+++ 2024 - 07 -  July/functional_programming.py
@@ -59,4 +59,4 @@ print(reduce(lambda x,y : x if x>y else y, arr1))
 print(reduce(lambda x,y : x if x<y else y, arr1))
 
 print(list(filter(lambda x: x&1, arr1)))
-print(filter(lambda x: "Odd" if x&1 == 1 else "even", arr1))
+print(list(filter(lambda x: "Odd" if x&1 == 1 else "even", arr1)))
