b313dd6d72678f17d30b85f43ba36a6d65c3adb9
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-09 01:21:01 +0530

    Auto-commit

--- 2024 - 07 -  July/functional_programming.py
+++ 2024 - 07 -  July/functional_programming.py
@@ -63,7 +63,11 @@ print(list(map(lambda x: "Odd" if x&1 == 1 else "even", arr1))) #print odd or ev
 
 
 #find the unique element (which is only one occurance in list) from a list of numbers
-arr1 = [1,1,5,4,3,3,5,6,7,2,7,6,2]
+"""arr1 = [1,1,5,4,3,3,5,6,7,2,7,6,2]
 print(reduce(lambda x,y : x^y, arr1))
-print(reduce(lambda x,y : x if x>y else y, arr1))
+print(reduce(lambda x,y : x if x>y else y, arr1))"""
+
+arr = [1,1,5,4,3,3,5,6,7,2,7,6,2]
+for num in arr:
+        print(num)
 
