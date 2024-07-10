b75ae2f15cc1f9b205092c5118d82ce1b8bb8797
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-11 00:45:04 +0530

    Auto-commit

--- 2024 - 07 -  July/n_meeting_in_one_room.py
+++ 2024 - 07 -  July/n_meeting_in_one_room.py
@@ -3,8 +3,6 @@ def max_meet(arr):
 
 start_time = [1, 3, 0, 5, 8, 5]
 end_time = [ 2, 4, 6, 7, 9, 9]
-pairs = []
-for key , value in (start_time, end_time):
-    pairs.append(key,value)
+pairs = start_time+end_time
 
 print(pairs)
