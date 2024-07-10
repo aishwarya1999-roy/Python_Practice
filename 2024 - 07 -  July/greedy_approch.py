f38b2dcb5fd4b7ce40c976bdc906dfec239b91be
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-10 19:23:18 +0530

    Auto-commit

--- 2024 - 07 -  July/greedy_approch.py
+++ 2024 - 07 -  July/greedy_approch.py
@@ -3,24 +3,3 @@
 """Greedy approach is good when the problem size is large and 
 attempts at arriving at the best solution will end up being more 
 costly than arriving at an acceptable optimal solution"""
-
-#coin cahnge problem
-
-def coin_change(arr,n):
-    arr.sort(reverse=True)
-    rem = n
-    coun = 0
-    coins_used = {denom: 0 for denom in arr}
-    for i in arr:
-        while rem >= i:
-            rem = rem-i
-            coun +=1
-            coins_used[i]+=1
-    print("Minimum number of coins needed:", coun)
-    for coin, count in coins_used.items():
-        if count > 0:
-            print(f"{count} coin(s) of {coin}$")
-
-arr = [1,2,5,6]
-n = 8
-coin_change(arr,n)
