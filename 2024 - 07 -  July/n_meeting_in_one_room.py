9032c9eb6fbce5b7446827741b69133ed75a0639
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-13 13:14:57 +0530

    Auto-commit

--- 2024 - 07 -  July/n_meeting_in_one_room.py
+++ 2024 - 07 -  July/n_meeting_in_one_room.py
@@ -1,6 +1,7 @@
 def max_meet(arr):
     count = 0
-    
+    for i in range(0,len(arr)):
+        if arr[i]
     return count
 
 
@@ -8,7 +9,7 @@ start_time = [3, 1, 8, 5, 0, 5]
 end_time = [ 4, 2, 9, 7, 6, 9]
 # [(1, 2), (3, 4), (0, 6), (5, 7), (8, 9), (5, 9)]
 sorted_pairs = sorted(zip(start_time, end_time), key=lambda x: x[1])
-print(sorted_pairs)
+print(sorted_pairs[1])
 
 print(max_meet(sorted_pairs))
 
