adb01a7e220ff2971f0c55c32c566e8ae662d331
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-13 13:12:28 +0530

    Auto-commit

--- 2024 - 07 -  July/n_meeting_in_one_room.py
+++ 2024 - 07 -  July/n_meeting_in_one_room.py
@@ -1,13 +1,13 @@
 def max_meet(arr):
     count = 0
-
+    
     return count
 
 
 start_time = [3, 1, 8, 5, 0, 5]
 end_time = [ 4, 2, 9, 7, 6, 9]
 sorted_pairs = sorted(zip(start_time, end_time), key=lambda x: x[1])
-print(type(sorted_pairs))
+print(sorted_pairs)
 
 print(max_meet(sorted_pairs))
 
