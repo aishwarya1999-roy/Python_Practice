51f778d776ec0a729561ee7f37ea817dab4a8044
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-13 13:31:15 +0530

    Auto-commit

--- 2024 - 07 -  July/n_meeting_in_one_room.py
+++ 2024 - 07 -  July/n_meeting_in_one_room.py
@@ -6,8 +6,6 @@ def max_meet(arr):
             count+=1
             end = arr[i+1][1]
     return count
-
-
 start_time = [3, 1, 8, 5, 0, 5]
 end_time = [ 4, 2, 9, 7, 6, 9]
 # [(1, 2), (3, 4), (0, 6), (5, 7), (8, 9), (5, 9)]
