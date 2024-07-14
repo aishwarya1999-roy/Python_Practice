fd6954e06f2986ac4d94ae8e3409942453a979c2
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-14 13:26:05 +0530

    Auto-commit

--- 2024 - 07 -  July/Shop_in_Candy_Store.py
+++ 2024 - 07 -  July/Shop_in_Candy_Store.py
@@ -3,7 +3,8 @@
 def candy_shop(N, price, K):
     max_money = 0
     min_money = price[0]
-    
+    for i in range(1,len(price))
+
 
     return min_money
 
@@ -12,6 +13,6 @@ def candy_shop(N, price, K):
 
 
 N = 4
-price = [3,2,1,4] # 1, 2, 3, 4
+price = [3,2,1,4,6] # 1, 2, 3, 4, 6
 K = 2
 print(candy_shop(N, sorted(price), K))
