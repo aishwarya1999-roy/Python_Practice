6b1746b9bcbc27dc0dde7e470a31d4235f8cc40d
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-14 12:48:10 +0530

    Auto-commit

--- 2024 - 07 -  July/n_meeting_in_one_room.py
+++ 2024 - 07 -  July/n_meeting_in_one_room.py
@@ -1,4 +1,4 @@
-def max_meet(arr):
+"""def max_meet(arr):
     arr.sort(key=lambda x: x[1])
     count = 1
     end = arr[0][1]
@@ -10,5 +10,29 @@ def max_meet(arr):
 start_time = [3, 1, 8, 5, 0, 5]
 end_time = [ 4, 2, 9, 7, 6, 9]
 meetings = list(zip(start_time, end_time))
-print(max_meet(meetings))
+print(max_meet(meetings))"""
 
+
+
+
+
+###################
+
+def maxMeetings(start_time, end_time):
+    meetings = [(start_time[i], end_time[i], i+1) for i in range(len(start_time))]
+    meetings.sort(key=lambda x: x[1])
+    selected_meetings = []
+    last_finish_time = -1
+    
+    for start, finish, index in meetings:
+        if start > last_finish_time:
+            selected_meetings.append(index)
+            last_finish_time = finish
+            
+    return sorted(selected_meetings)
+
+S = [12, 6, 16, 12, 6, 9 , 16, 6, 17, 5]
+F =  [ 17, 13, 16, 18, 17, 10, 18, 12, 18, 11]
+
+selected_meetings = maxMeetings(S, F)
+print("The meetings that can be accommodated are:", selected_meetings)
