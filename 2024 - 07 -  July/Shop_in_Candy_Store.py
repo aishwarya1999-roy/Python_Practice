7301fc6458233d4e8e98142daba9e9d897685b65
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-14 15:02:06 +0530

    Auto-commit

--- 2024 - 07 -  July/Shop_in_Candy_Store.py
+++ 2024 - 07 -  July/Shop_in_Candy_Store.py
@@ -21,12 +21,9 @@ print(candy_shop(N, price, K))
 import math
 
 def min_cost_to_buy_all_candies(N, K, candies):
-    # Sort the candies by their prices in ascending order
     candies.sort()
-    
-    # Calculate the number of candies we need to actually buy
     num_candies_to_buy = math.ceil(N / (K + 1))
-    
+    print(num_candies_to_buy)
     # Calculate the minimum cost by summing the prices of the cheapest candies
     min_cost = sum(candies[:num_candies_to_buy])
     
@@ -37,9 +34,3 @@ N = 4
 K = 2
 candies = [3, 2, 1, 4]
 print(min_cost_to_buy_all_candies(N, K, candies))  # Output: 3
-
-# Example 2
-N = 5
-K = 4
-candies = [3, 2, 1, 4, 5]
-print(min_cost_to_buy_all_candies(N, K, candies))  # Output: 1
