484213be294c45a6d11e04d0f74bc191592e5580
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-14 14:35:32 +0530

    Auto-commit

--- 2024 - 07 -  July/Shop_in_Candy_Store.py
+++ 2024 - 07 -  July/Shop_in_Candy_Store.py
@@ -5,8 +5,7 @@ def candy_shop(N, price, K):
     i=0
     while i<N:
         min_money += price[i]
-        [price.pop() for i in range(K) if len(price)>1]
-        i+=1
+        i += (K + 1)
     return min_money
 N = int(input("Enter amount for N : "))
 K = int(input("Enter amount for K : "))
