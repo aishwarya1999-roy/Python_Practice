11ea874428457e5509c7339aa2706323ce6449d4
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-14 14:15:04 +0530

    Auto-commit

--- 2024 - 07 -  July/Shop_in_Candy_Store.py
+++ 2024 - 07 -  July/Shop_in_Candy_Store.py
@@ -3,13 +3,12 @@
 def candy_shop(N, price, K):
     max_money = 0
     min_money = 0
-    aaa = []
     i=0
     while len(price)>0:
         print("Before : ",i, price)
         min_money += price[i]
-        aaa.append([price.pop() for i in range(K)])
-        print("After : " , aaa)
+        [price.pop() for i in range(K)]
+        print("After : " , price)
         i+=1
     return min_money
 N = 4
