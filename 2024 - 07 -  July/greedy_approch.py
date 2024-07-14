eed2d9f4bc8da1a83de1e58144db1b4c8821d0cb
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-14 12:50:46 +0530

    Auto-commit

--- 2024 - 07 -  July/greedy_approch.py
+++
@@ -1,5 +0,0 @@
-# finding min or max
-
-"""Greedy approach is good when the problem size is large and 
-attempts at arriving at the best solution will end up being more 
-costly than arriving at an acceptable optimal solution"""
