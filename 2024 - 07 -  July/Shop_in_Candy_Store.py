a275e9ffb52f2cc3c45434477c4b949335b6fc20
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-14 14:35:00 +0530

    Auto-commit

--- 2024 - 07 -  July/Shop_in_Candy_Store.py
+++ 2024 - 07 -  July/Shop_in_Candy_Store.py
@@ -1,10 +1,9 @@
 # N types  candies
 
 def candy_shop(N, price, K):
-    max_money = 0
     min_money = 0
     i=0
-    while len(price)>1:
+    while i<N:
         min_money += price[i]
         [price.pop() for i in range(K) if len(price)>1]
         i+=1
