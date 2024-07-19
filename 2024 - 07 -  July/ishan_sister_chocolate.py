044b699c33d144fd66019aab036e9954f9d26ae5
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-19 07:47:38 +0530

    Auto-commit

---
+++ 2024 - 07 -  July/ishan_sister_chocolate.py
@@ -0,0 +1,19 @@
+#min
+def chocolate(arr, N):
+    arr.sort(reverse = True)
+    return arr
+
+
+N = 5
+arr = [5,3,1,6,9]
+print(chocolate(arr, N))
+
+#sort
+def chocolate(arr, N):
+    arr.sort(reverse = True)
+    return arr
+
+
+N = 5
+arr = [5,3,1,6,9]
+print(chocolate(arr, N))
