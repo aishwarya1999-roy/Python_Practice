7e275942df4932b9257a2ab858b9466650a2c7e7
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-20 13:23:32 +0530

    Auto-commit

--- 2024 - 07 -  July/servive_on_island.py
+++ 2024 - 07 -  July/servive_on_island.py
@@ -1,16 +1,13 @@
 import math
 
 def minfood(N, S, M):
-    sunday = S//7
-    food = math.ceil((M*S)/N)
+    weeks = S//7
+    extra_days = S%7
+    if M>N:
+        return -1
+    
+    food =(M*S + N-1)//N
 
-    if sunday>0:
-        if food >= S-sunday:
-            return -1
-        else:
-            return food
-    else:
-        return food
 
 N = 2 # max food you can buy each day
 S = 5 # days to servive
