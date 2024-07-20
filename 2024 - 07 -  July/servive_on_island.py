65f1751c8e70f0a755c987a92f85ff6a79566308
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-20 13:33:18 +0530

    Auto-commit

--- 2024 - 07 -  July/servive_on_island.py
+++ 2024 - 07 -  July/servive_on_island.py
@@ -23,8 +23,8 @@ def minimum_buying_days(N, S, M):
     return min_buying_days
 
 # Example usage:
-N = 9  # Max food Geekina can buy each day
-S = 10  # Number of days Geekina needs to survive
-M = 8   # Food Geekina needs each day
+N = 2  # Max food Geekina can buy each day
+S = 5 # Number of days Geekina needs to survive
+M = 2   # Food Geekina needs each day
 
 print(minimum_buying_days(N, S, M))  # Should return the minimum number of days needed to buy food or -1 if not possible
