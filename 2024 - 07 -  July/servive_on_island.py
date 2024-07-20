e06471b611d631024f5b57c0f15c15eb8aa4f35a
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-20 12:37:44 +0530

    Auto-commit

--- 2024 - 07 -  July/servive_on_island.py
+++ 2024 - 07 -  July/servive_on_island.py
@@ -6,7 +6,8 @@ def minfood(N, S, M):
         return -1
     else:
         food = math.ceil((M*S)/N)
-    if food >= S-1
+    if food >= S-1:
+        return -1
     return food
 
 N = 10 # max food you can buy each day
