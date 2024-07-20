dffec84e87cb88a286da978162479e98977584e4
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-20 14:29:29 +0530

    Auto-commit

--- 2024 - 07 -  July/div by 7.py
+++ 2024 - 07 -  July/div by 7.py
@@ -19,3 +19,22 @@ S = 10
 M = 8
 
 print(minimum_buying_days(N, S, M))
+
+int M = 8,N = 9,S = 10;
+int sunday = S/7;
+int total_food_required = S * M;
+int buying_day = S - sunday
+int min_buying_days = 0
+
+if(total_food_required % N) == 0 {
+    min_buying_days = total_food_required/N
+}
+else{
+    min_buying_days = total_food_required/N + 1
+}
+if(min_buying_days <= buying_day)
+    return min_buying_days;
+else
+    return -1;
+
+print(min_buying_days)
