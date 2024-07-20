a5ccf4ba94a746a16bfcafb2614593fdbbd46d0a
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-20 13:37:57 +0530

    Auto-commit

---
+++ 2024 - 07 -  July/servive_on_island.py
@@ -0,0 +1,20 @@
+def minimum_buying_days(N, S, M):
+    total_food_required = S * M
+    weeks = S // 7
+    extra_days = S % 7
+    max_buying_days = weeks * 6 + extra_days
+    if M > N:
+        return -1
+    min_buying_days = (total_food_required + N - 1) // N
+    print(min_buying_days)
+    if min_buying_days > max_buying_days:
+        return -1
+    
+    return min_buying_days
+
+
+N = 2
+S = 5
+M = 2
+
+print(minimum_buying_days(N, S, M)) 
