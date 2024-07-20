e14012d1a66217692cb6ed7812b67055ebc10e4a
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-20 13:42:34 +0530

    Auto-commit

--- 2024 - 07 -  July/servive_on_island.py
+++ 2024 - 07 -  July/servive_on_island.py
@@ -9,9 +9,8 @@ def minimum_buying_days(N, S, M):
     print(min_buying_days)
     print(max_buying_days)
     print(weeks)
-    if weeks > 0:
-        if min_buying_days > max_buying_days:
-            return -1
+    if weeks > 0 and min_buying_days > max_buying_days:
+        return -1
     
     return min_buying_days
 
