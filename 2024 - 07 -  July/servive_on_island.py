bc124de4db187518fdb2288acc6df7c49818e8f2
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-20 14:39:11 +0530

    Auto-commit

--- 2024 - 07 -  July/servive_on_island.py
+++ 2024 - 07 -  July/servive_on_island.py
@@ -1,25 +1,21 @@
 def minimum_buying_days(N, S, M):
-    # Calculate total food required for S days
     total_food_required = S * M
-    # Calculate the number of Sundays
-    sundays = S // 7
-    # Calculate the number of available buying days
-    buying_day = S - sundays
-    
-    # Calculate minimum buying days required
-    if total_food_required % N == 0:
-        min_buying_days = total_food_required // N
-    else:
-        min_buying_days = total_food_required // N + 1 
-    
-    # Check if the minimum buying days is within the available buying days
-    if min_buying_days > buying_day:
-        return -1
-    else:
-        return min_buying_days
+    sunday= S // 7
+    buying_day = S - sunday
+    min_buying_days = 0
 
-N = 24
-S = 30
-M = 20
+    if total_food_required % N ==0:
+        min_buying_days = total_food_required//N
+    else:
+        min_buying_days = total_food_required//N + 1 
+    
+    if min_buying_days <= buying_day:
+        return min_buying_days
+    else:
+        return -1
+
+N = 9
+S = 10
+M = 8
 
 print(minimum_buying_days(N, S, M))
