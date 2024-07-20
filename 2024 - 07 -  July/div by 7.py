f9efa112b585b06518ecab830242b02a7356b595
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-20 16:24:07 +0530

    Auto-commit

--- 2024 - 07 -  July/div by 7.py
+++ 2024 - 07 -  July/div by 7.py
@@ -5,13 +5,13 @@ def minimum_buying_days(N, S, M):
     if (N-M)*6 < M and S > 6:
         return -1
     if total_food%N == 0 and N == M and S > 6:
-        ans = -1
+        food = -1
 
     elif total_food%N == 0:
-        ans = total_food//N
+        food = total_food//N
     else:
-        ans = total_food//N + 1
-    return ans
+        food = total_food//N + 1
+    return food
 
 print(minimum_buying_days(9, 10, 8))
 print(minimum_buying_days(2, 5, 2))
