fed4d74f0cc1d9c9df52c1b4035690c8fefff641
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-20 14:31:55 +0530

    Auto-commit

--- 2024 - 07 -  July/div by 7.py
+++ 2024 - 07 -  July/div by 7.py
@@ -1,14 +1,19 @@
 def minimum_buying_days(N, S, M):
+    # Calculate total food required for S days
     total_food_required = S * M
-    sunday= S // 7
+    # Calculate the number of Sundays
+    sunday = S // 7
+    # Calculate the number of available buying days
     buying_day = S - sunday
     min_buying_days = 0
 
-    if total_food_required % N ==0:
-        min_buying_days = total_food_required//N
+    # Calculate minimum buying days required
+    if total_food_required % N == 0:
+        min_buying_days = total_food_required // N
     else:
-        min_buying_days = total_food_required//N + 1 
+        min_buying_days = total_food_required // N + 1 
     
+    # Check if the minimum buying days is within the available buying days
     if min_buying_days <= buying_day:
         return min_buying_days
     else:
@@ -19,22 +24,3 @@ S = 10
 M = 8
 
 print(minimum_buying_days(N, S, M))
-
-int M = 8,N = 9,S = 10;
-int sunday = S/7;
-int total_food_required = S * M;
-int buying_day = S - sunday
-int min_buying_days = 0
-
-if(total_food_required % N) == 0 {
-    min_buying_days = total_food_required/N
-}
-else{
-    min_buying_days = total_food_required/N + 1
-}
-if(min_buying_days <= buying_day)
-    return min_buying_days;
-else
-    return -1;
-
-print(min_buying_days)
