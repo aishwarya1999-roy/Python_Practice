78d583892def8c6933b2cd9adb9b351a4b8690c3
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-20 16:19:06 +0530

    Auto-commit

--- 2024 - 07 -  July/div by 7.py
+++ 2024 - 07 -  July/div by 7.py
@@ -1,15 +1,15 @@
 def minimum_buying_days(N, S, M):
     if N < M:
             return -1
-        if (N-M)*6 < M and S > 6:
-            return -1
-        if (S*M)%N == 0 and N == M and S > 6:
-            ans = -1
-        elif (S*M)%N == 0:
-            ans = (S*M)//N
-        else:
-            ans = (S*M)//N + 1
-        return ans
+    if (N-M)*6 < M and S > 6:
+        return -1
+    if (S*M)%N == 0 and N == M and S > 6:
+        ans = -1
+    elif (S*M)%N == 0:
+        ans = (S*M)//N
+    else:
+        ans = (S*M)//N + 1
+    return ans
 
 print(minimum_buying_days(9, 10, 8))
 print(minimum_buying_days(2, 5, 2))
