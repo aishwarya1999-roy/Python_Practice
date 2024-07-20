242134ae0db3bdfed57a0669b29180605ef5dc00
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-20 13:28:47 +0530

    Auto-commit

--- 2024 - 07 -  July/div by 7.py
+++ 2024 - 07 -  July/div by 7.py
@@ -1,3 +1,17 @@
-S= 5
-sunday = S//7
-print(sunday)
+def minimum_buying_days(N, S, M):
+    total_food_required = S * M
+    sunday= S // 7
+    if M > N:
+        return -1
+    min_buying_days = (total_food_required + N - 1) // N
+    if min_buying_days > S-sunday:
+        return -1
+    
+    return min_buying_days
+
+# Example usage:
+N = 16  # Max food Geekina can buy each day
+S = 10  # Number of days Geekina needs to survive
+M = 2   # Food Geekina needs each day
+
+print(minimum_buying_days(N, S, M))  # Should return the minimum number of days needed to buy food or -1 if not possible
