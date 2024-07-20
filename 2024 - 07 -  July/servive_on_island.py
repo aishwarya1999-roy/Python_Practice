29e9af9f61a5ecfdaddb9a0095e5dcc572d9cd13
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-20 13:12:06 +0530

    Auto-commit

--- 2024 - 07 -  July/servive_on_island.py
+++ 2024 - 07 -  July/servive_on_island.py
@@ -7,7 +7,7 @@ def minfood(N, S, M):
         return -1
     return food
 
-N = 9 # max food you can buy each day
-S = 10 # days to servive
-M = 8 # unit of food each day to eat
+N = 2 # max food you can buy each day
+S = 5# days to servive
+M = 2 # unit of food each day to eat
 print(minfood(N, S, M))
