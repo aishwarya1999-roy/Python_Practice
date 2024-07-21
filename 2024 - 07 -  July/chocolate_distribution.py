7325dc8ca2c9811aea4245fdc1c3b9863a165f57
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-21 14:06:25 +0530

    Auto-commit

--- 2024 - 07 -  July/chocolate_distribution.py
+++ 2024 - 07 -  July/chocolate_distribution.py
@@ -3,6 +3,10 @@ def chocolate(arr,N,M):
         arr.sort()
         i = 0
         j = M-1
+
+        while j<N:
+            diff = arr[i] - arr[j]
+
     return arr
 
 arr = [7, 3, 2, 4, 9, 12, 56] #[1, 3, 4, 7, 9, 9, 12, 56]
