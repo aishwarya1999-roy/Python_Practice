ca0102b1996478bd9a898802d1202fc62ea26a2a
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-21 15:28:51 +0530

    Auto-commit

--- 2024 - 07 -  July/minimum_cost_of_ropes.py
+++ 2024 - 07 -  July/minimum_cost_of_ropes.py
@@ -3,7 +3,7 @@ def minCost(arr,n) :
     heapq.heapify(arr)
     
     total_cost = 0
-    while n > 1:
+    while len(arr) > 1:
         first = heapq.heappop(arr)
         second = heapq.heappop(arr)
         cost = first + second
