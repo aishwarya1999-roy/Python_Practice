0af607547d2ad67fb5fba240854629deb4b93510
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-21 15:27:18 +0530

    Auto-commit

--- 2024 - 07 -  July/minimum_cost_of_ropes.py
+++ 2024 - 07 -  July/minimum_cost_of_ropes.py
@@ -3,14 +3,9 @@ def minCost(arr,n) :
     heapq.heapify(arr)
     
     total_cost = 0
-    
-    # While there is more than one rope
     while n > 1:
-        # Extract the two smallest ropes
         first = heapq.heappop(arr)
         second = heapq.heappop(arr)
-        
-        # Calculate the cost of connecting them
         cost = first + second
         total_cost += cost
         
