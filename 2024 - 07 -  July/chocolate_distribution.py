610121bce892ce117845c90c2e363a9106acf090
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-21 13:39:34 +0530

    Auto-commit

--- 2024 - 07 -  July/chocolate_distribution.py
+++ 2024 - 07 -  July/chocolate_distribution.py
@@ -3,7 +3,7 @@ def chocolate(arr,N,M):
         sorted(arr)
         print(arr[i],arr[i+1],arr[i+2])
 
-    return sorted(arr)
+    return arr
 
 arr = [3, 4, 1, 9, 56, 7, 9, 12] #[1, 3, 4, 7, 9, 9, 12, 56]
 N = 8
