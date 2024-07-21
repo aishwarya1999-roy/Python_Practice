d81fbf0ffed65bfb8d7caab95bf625f77d9e1b1b
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-21 12:45:24 +0530

    Auto-commit

--- 2024 - 07 -  July/reverse_string.py
+++ 2024 - 07 -  July/reverse_string.py
@@ -1,6 +1,31 @@
+#Reverse Words
+
 def reverseWords(S):
-    return S[::-1]
+    a = ".".join(S.split(".")[::-1])
+    return a
 
 S = "i.like.this.program.very.much"
 
 print(reverseWords(S))
+
+#Reverse Words - without using the built-in split, join, and reverse functions
+"""def reverseWords(S):
+    result = ""
+    words = []
+    curr_word = ""
+    for i in range(len(S)):
+        if(S[i] == '.'):
+            words.append(curr_word)
+            curr_word = ""
+        else:
+            curr_word += S[i]
+    words.append(curr_word)
+    for i in range(len(words) - 1, -1, -1):
+        result += words[i]
+        if(i):
+            result += "."
+    return result
+
+S = "i.like.this.program.very.much"
+
+print(reverseWords(S))"""
