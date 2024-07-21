2bccccf4f335db58290ddf04ad5aec7bf1c13328
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-21 15:15:31 +0530

    Auto-commit

--- 2024 - 07 -  July/minimum_cost_of_ropes.py
+++ 2024 - 07 -  July/minimum_cost_of_ropes.py
@@ -1,9 +1,9 @@
 def minCost(arr,n) :
     arr.sort()
-    sum = arr[0]
+    addd = arr[0]
     aa = []
     for i in range(n-1):
-        sum = sum + arr[i+1]
+        addd = addd + arr[i+1]
         aa.append(sum)
 
     return sum(aa)
