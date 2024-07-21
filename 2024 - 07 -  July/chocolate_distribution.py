d5674e588ae55ab7b15781f588388674a41f0a7c
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-21 13:42:22 +0530

    Auto-commit

--- 2024 - 07 -  July/chocolate_distribution.py
+++ 2024 - 07 -  July/chocolate_distribution.py
@@ -1,8 +1,8 @@
 def chocolate(arr,N,M):
     for i in range(len(arr)-2):
         arr.sort()
-        print("min : " , min(arr[i],arr[i+1],arr[i+2]), end=" ")
-        print("max : " ,max(arr[i],arr[i+1],arr[i+2]))
+        print("min :" , min(arr[i],arr[i+1],arr[i+2]), end=" ")
+        print("max :" ,max(arr[i],arr[i+1],arr[i+2]))
     return arr
 
 arr = [3, 4, 1, 9, 56, 7, 9, 12] #[1, 3, 4, 7, 9, 9, 12, 56]
