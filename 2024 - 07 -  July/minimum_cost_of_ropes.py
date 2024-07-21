aca7b8a484f2d50f3d3e108a83ae7c19aaa28cd8
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-21 15:26:10 +0530

    Auto-commit

--- 2024 - 07 -  July/minimum_cost_of_ropes.py
+++ 2024 - 07 -  July/minimum_cost_of_ropes.py
@@ -1,14 +1,14 @@
 import heapq
 def minCost(arr,n) :
-    first = heapq.heappop(ropes)
-    second = heapq.heappop(ropes)
+    first = heapq.heappop(arr)
+    second = heapq.heappop(arr)
         
     # Calculate the cost of connecting them
     cost = first + second
     total_cost += cost
         
     # Insert the new rope back into the heap
-    heapq.heappush(ropes, cost)
+    heapq.heappush(arr, cost)
     return first
 
 arr = [4, 3, 2, 6] # 2, 3, 4, 6
