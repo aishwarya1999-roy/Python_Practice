5dffe03ebf6faaaa4de4d3fc1c632c0cf7951311
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-27 12:40:57 +0530

    Auto-commit

--- 2024 - 07 -  July/find_pair.py
+++ 2024 - 07 -  July/find_pair.py
@@ -3,13 +3,29 @@
 #Given a set of numbers, find a pair of numbers that add upto a certain target, 
 # ex [1, 2, 3, 9] ,and taregt 10, identify that 1 & 9 add upto 10.
 
-A = [1, 2, 3, 9]
+
+#Brute force approch = O(n^2)
+"""A = [1, 2, 3, 9]
 
 m = 10
 
 n= len(A)
 for i in range(n):
-    for j in range(n):
+    for j in range(i+1,n):
         if A[i]+A[j] == m:
-            print(A[i], A[j], "sum = ", A[i]+A[j])
+                print(A[i], A[j])"""
 
+#Using Hash set = O(n)
+def find_pair(A, m):
+    seen  = set()
+    for num in A:
+        complement = m - num
+        if complement in seen:
+            print(num, complement)
+            return  # Return after finding the first pair
+        seen.add(num)
+        
+
+A = [1, 2, 3, 9]
+m = 10
+find_pair(A, m)
--- 2024 - 07 -  July/heap.py
+++ 2024 - 07 -  July/heap.py
@@ -5,3 +5,13 @@ arr = [4, 3, 2, 1, 6, 7, 1]
 heapq.heapify(arr)
 
 print(arr)
+
+print(heapq.heappop(arr))
+print(arr)
+heapq.heappush(arr, 1)
+print(arr)
+
+heapq._heapify_max(arr)
+print(arr)
+print(heapq._heappop_max(arr))
+print(arr)
