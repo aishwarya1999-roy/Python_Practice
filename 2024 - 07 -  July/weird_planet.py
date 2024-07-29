1e41fc5eb43ef45aadde22fa48fe697c0acdd589
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-29 17:46:03 +0530

    Auto-commit

--- 2024 - 07 -  July/weird_planet.py
+++ 2024 - 07 -  July/weird_planet.py
@@ -1,33 +1,26 @@
-def process_customer_requests(Hourperday, supervisors, customers):
-    results = []
-    for customer in customers:
-        customer_height, visit_timing = customer
-        
-        if visit_timing > Hourperday:
-            results.append("No")
-            continue
-        
-        not_accepted = False
-        for supervisor in supervisors:
-            supervisor_height, supervisor_shif_start, supervisor_shif_end = supervisor
-            if customer_height > supervisor_height or visit_timing < supervisor_shif_start or visit_timing > supervisor_shif_end:
-                continue
+def process_customer_requests(H, supervisor_data, customer_requests):
+    on_duty_heights = [-1] * (H + 1)
+    for height, start, end in supervisor_data:
+        for hour in range(start, end + 1):
+            if on_duty_heights[hour] == -1:
+                on_duty_heights[hour] = height
             else:
-                not_accepted = True
-                break
-        
-        if not_accepted:
-            results.append("No")
+                on_duty_heights[hour] = max(on_duty_heights[hour], height)
+
+    results = []
+    for cust_height, visit_time in customer_requests:
+        if on_duty_heights[visit_time] == -1 or cust_height > on_duty_heights[visit_time]:
+            results.append("YES")
         else:
-            results.append("Yes")
-    
+            results.append("NO")
+
     return results
 
-Hourperday = 12
-supervisors = [(50, 2, 5), (40, 3, 6), (60, 2, 7)]  # [(supervisor_height, supervisor_shif_start, supervisor_shif_end)]
-customers = [(10, 1), (20, 2), (41, 4), (55, 5), (100, 8)]  # [(customer_height, visit_timing)]
+# Example usage:
+H = 10
+supervisor_data = [(50, 2, 5), (40, 3, 6), (60, 2, 7)]
+customer_requests = [(10, 1), (10, 2), (50, 5), (51, 6), (100, 10)]
 
-results = process_customer_requests(Hourperday, supervisors, customers)
-
+results = process_customer_requests(H, supervisor_data, customer_requests)
 for result in results:
-        print(result)
+    print(result)
