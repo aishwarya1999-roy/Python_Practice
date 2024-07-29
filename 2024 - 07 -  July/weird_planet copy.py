e2b9cd7fd93f1af825e9e61d10c2f5b5e9af150d
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-29 17:35:17 +0530

    Auto-commit

--- 2024 - 07 -  July/weird_planet copy.py
+++ 2024 - 07 -  July/weird_planet copy.py
@@ -1,4 +1,4 @@
-def process_customer_requests(H, S, R, supervisor_data, customer_requests):
+def process_customer_requests(H, supervisor_data, customer_requests):
     # Initialize an array to store the height of the supervisor on duty at each hour
     on_duty_heights = [-1] * (H + 1)
 
@@ -22,11 +22,9 @@ def process_customer_requests(H, S, R, supervisor_data, customer_requests):
 
 # Example usage:
 H = 10
-S = 1
-R = 5
 supervisor_data = [(50, 2, 5), (40, 3, 6), (60, 2, 7)]
 customer_requests = [(10, 1), (10, 2), (50, 5), (51, 6), (100, 10)]
 
-results = process_customer_requests(H, S, R, supervisor_data, customer_requests)
+results = process_customer_requests(H, supervisor_data, customer_requests)
 for result in results:
     print(result)
