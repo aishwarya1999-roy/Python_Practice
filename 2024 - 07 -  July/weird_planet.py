ed77da2b81feb8c2eef6073d1408b40d3768f828
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-29 18:29:07 +0530

    Auto-commit

--- 2024 - 07 -  July/weird_planet.py
+++ 2024 - 07 -  July/weird_planet.py
@@ -2,7 +2,7 @@ def process_customer_requests(H, supervisor_data, customer_requests):
     on_duty_heights = [-1] * (H + 1)
     height, start, end = supervisor_data
     max_height = max(height)
-    print(max_height)
+    print(height)
     results = []
     
 
