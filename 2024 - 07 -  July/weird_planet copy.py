2157b8644395798f082909d828b92ca44a04b054
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-29 17:30:53 +0530

    Auto-commit

--- 2024 - 07 -  July/weird_planet copy.py
+++ 2024 - 07 -  July/weird_planet copy.py
@@ -1,25 +1,9 @@
 def process_customer_requests(Hourperday, supervisors, customers):
     results = []
+
+
     for customer in customers:
-        customer_height, visit_timing = customer
         
-        if visit_timing > Hourperday:
-            results.append("No")
-            continue
-        
-        not_accepted = False
-        for supervisor in supervisors:
-            supervisor_height, supervisor_shif_start, supervisor_shif_end = supervisor
-            if customer_height > supervisor_height or visit_timing < supervisor_shif_start or visit_timing > supervisor_shif_end:
-                continue
-            else:
-                not_accepted = True
-                break
-        
-        if not_accepted:
-            results.append("No")
-        else:
-            results.append("Yes")
     
     return results
 
