ef1882a380b16bad570cad16e7761fda31d625b6
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-29 18:05:52 +0530

    Auto-commit

--- 2024 - 07 -  July/weird_planet copy.py
+++ 2024 - 07 -  July/weird_planet copy.py
@@ -22,7 +22,7 @@ S = 2
 R = 5
 string2 = [input() for _ in range(S)]
 supere  = [tuple(map(int, s.split())) for s in string2]
-print(string2)
+print(supere)
 
 supervisor_data = [(50, 2, 5), (40, 3, 6), (60, 2, 7)]
 customer_requests = [(10, 1), (10, 2), (50, 5), (51, 6), (100, 10)]
