a3d418e6fd2c94dac4a5c9787cf5f6de3f33c8cf
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-11 09:09:20 +0530

    Auto-commit

--- 2024 - 08 - Aug/recursion.py
+++ 2024 - 08 - Aug/recursion.py
@@ -1,10 +1,7 @@
 # factorial - recursion
-"""def fact(N):
+def fact(N):
     if N == 0:
         return 1
     return N * fact(N-1)
 N = int(input("N : "))
-print(fact(N))"""
-
-N = int(input("N : "))
-print(N)
+print(fact(N))
