3daf263e26952ffd0fb3791713fd75839f53c9c2
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-11 15:54:54 +0530

    Auto-commit

--- 2024 - 08 - Aug/recursion.py
+++ 2024 - 08 - Aug/recursion.py
@@ -16,9 +16,37 @@ power = int(input("power : "))
 print(power_function(num, power))"""
 
 #counting - recursion
-def power_function(num, power):
-    if power == 0:
-        return 1
-    return num * power_function(num, power-1)
+"""def counting(num):
+    if num == 0:
+        return 0
+    print(num)
+    return counting(num-1)
+    
 num = int(input("Number : "))
-print(power_function(num))
+print(counting(num))
+"""
+
+#fibo
+
+"""def fibonacci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+     
+    return fibonacci(n-1)+fibonacci(n-2)
+
+n = int(input())
+print(fibonacci(n))"""
+    
+#reaching home - level 0 recursion
+def power_function(dest, sour):
+    print("Source : ", sour, "Destination : ", dest)
+    if sour == dest:
+        return "Reached Home"
+    
+    return power_function(dest, sour+1)
+
+dest = int(input("Destination : "))
+sour = int(input("Source : "))
+print(power_function(dest, sour))
