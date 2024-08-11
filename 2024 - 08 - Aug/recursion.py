24f4ab16e0aef1075bfa769e834a9280cfedbac9
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-11 09:32:10 +0530

    Auto-commit

--- 2024 - 08 - Aug/recursion.py
+++ 2024 - 08 - Aug/recursion.py
@@ -10,7 +10,7 @@ print(fact(N))"""
 def power_function(num, power):
     if num == 0:
         return 1
-    return num * (num * power)
+    return num * (num * (power-1))
 num = int(input("Number : "))
 power = int(input("power : "))
 print(power_function(num, power))
