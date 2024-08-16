c652a94d65c36cda14ee9820b32b71438cc2e5a9
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-16 15:02:05 +0530

    Auto-commit

---
+++ 2024 - 08 - Aug/Recursion/array_sorted_or_not.py
@@ -0,0 +1,13 @@
+#array is sorted or not
+def sorted_or_not(arr,n):
+    print(arr)
+    if n==1 or n==0:
+        return True
+    if arr[0]>arr[1]:
+        return False
+    else:
+        ans = sorted_or_not(arr[1:],n-1)
+        return ans
+arr = [6,3,10,7,8]
+n = len(arr)
+print(sorted_or_not(arr,n))
