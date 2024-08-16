b023768f9cbe724690427201755e9491a6db3ba5
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-16 13:25:14 +0530

    Auto-commit

--- 2024 - 08 - Aug/recursion.py
+++ 2024 - 08 - Aug/recursion.py
@@ -1,12 +1,3 @@
-#print counting - recursion
-def counting(num):
-    if num == 0:
-        return 
-    print(num, end=" ")
-    return counting(num-1)
-    
-num = int(input("Number : ")) #5
-counting(num) # 5 4 3 2 1
 
 
 #array is sorted or not
