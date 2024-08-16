346f64876452d98e3700d73f8595a7f0657848bc
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-16 13:25:08 +0530

    Auto-commit

---
+++ 2024 - 08 - Aug/print_counting.py
@@ -0,0 +1,9 @@
+#print counting - recursion
+def counting(num):
+    if num == 0:
+        return 
+    print(num, end=" ")
+    return counting(num-1)
+    
+num = int(input("Number : ")) #5
+counting(num) # 5 4 3 2 1
