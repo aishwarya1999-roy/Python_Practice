65624ad5123b2ac9add934d49690ad7de3aa2b3d
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-16 14:49:45 +0530

    Auto-commit

--- 2024 - 08 - Aug/Recursion/array_sorted_or_not.py
+++ 2024 - 08 - Aug/Recursion/array_sorted_or_not.py
@@ -2,6 +2,8 @@
 def sorted_or_not(arr):
     if len(arr)==1 or len(arr)==0:
         return True
+    if arr[0]>arr[1]:
+        return False
     """if arr[i]>arr[i-1]:
         return True"""
     return sorted_or_not(arr)
