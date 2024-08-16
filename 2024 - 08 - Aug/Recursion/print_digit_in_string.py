30056417f43d683655705d8c78413e14584cb09e
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-16 13:21:11 +0530

    Auto-commit

---
+++ 2024 - 08 - Aug/Recursion/print_digit_in_string.py
@@ -0,0 +1,12 @@
+#print digit in string
+def say_digit(digits,arr):
+    if digits == 0:
+        return 
+    dig = digits%10
+    digits=digits//10
+    say_digit(digits, arr)
+    print(arr[dig], end=" ")
+
+arr = ['Zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+digits = int(input("Digits : "))
+say_digit(digits, arr)
