287a355dad115e1e68204a5c14cc85f48a5ba0b3
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-18 13:19:51 +0530

    Auto-commit

--- 2024 - 08 - Aug/Recursion/string_revearse.py
+++ 2024 - 08 - Aug/Recursion/string_revearse.py
@@ -2,8 +2,9 @@ def rev(ss, i, h):
     print("i: " , i, "h :",h)
     aa = list(ss)
     if i>h:
-        return aa
+        return
     aa[i],aa[h] = aa[h],aa[i]
+    print(aa)
     return rev(ss, i+1, h-1)
 
 ss = "abcdef"
