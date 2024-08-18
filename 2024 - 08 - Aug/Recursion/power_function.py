9afaee5cc5b1fb0fa1a32c18aa03a74810154e87
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-18 15:11:20 +0530

    Auto-commit

--- 2024 - 08 - Aug/Recursion/power_function.py
+++ 2024 - 08 - Aug/Recursion/power_function.py
@@ -1,13 +1,14 @@
-#power - recursion - way 1
-# def power_function(num, power):
-#     if power == 0:
-#         return 1
-#     if power == 1:
-#         return num
-#     return num * power_function(num, power-1)
-# num = int(input("Number : "))
-# power = int(input("power : "))
-# print(power_function(num, power))
+#power - recursion - way 1 (O(power))
+def power_function(num, power):
+    if power == 0:
+        return 1
+    if power == 1:
+        return num
+    return num * power_function(num, power-1)
+
+num = int(input("Number : "))
+power = int(input("power : "))
+print(power_function(num, power))
 
 #power - recursion - way2
 def power_function(num, power):
@@ -20,6 +21,7 @@ def power_function(num, power):
         return ans * ans
     else:
         return num * ans * ans
+    
 num = int(input("Number : "))
 power = int(input("power : "))
 print(power_function(num, power))
