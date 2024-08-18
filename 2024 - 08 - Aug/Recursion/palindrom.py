f2b35fb34c5a5fdeca74fd99e59a4f7ade19abf2
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-18 14:18:34 +0530

    Auto-commit

---
+++ 2024 - 08 - Aug/Recursion/palindrom.py
@@ -0,0 +1,9 @@
+def rev(ss, i, h):
+    if i>h:
+        return ss
+    aa = list(ss)
+    aa[i],aa[h] = aa[h],aa[i]
+    return rev("".join(aa), i+1, h-1)
+
+ss = "abcdef"
+print(rev(ss,0,len(ss)-1))
