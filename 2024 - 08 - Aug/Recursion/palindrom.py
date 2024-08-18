da208121742d5f123cc368930d4d36d024f8c6d2
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-18 14:45:01 +0530

    Auto-commit

--- 2024 - 08 - Aug/Recursion/palindrom.py
+++ 2024 - 08 - Aug/Recursion/palindrom.py
@@ -1,6 +1,20 @@
+# def rev(ss, i, h):
+#     if i>h:
+#         return ss
+#     aa = list(ss)
+#     aa[i],aa[h] = aa[h],aa[i]
+#     return rev("".join(aa), i+1, h-1)
+
+# ss = "abbaa"
+# ans = (rev(ss,0,len(ss)-1))
+# if ans==ss:
+#     print("Pallindrom")
+# else:
+#     print("Not Pallindrom")
+
 def rev(ss, i, h):
     if i>h:
-        return ss
+        return
     aa = list(ss)
     aa[i],aa[h] = aa[h],aa[i]
     return rev("".join(aa), i+1, h-1)
