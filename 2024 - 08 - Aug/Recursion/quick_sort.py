f4fd1068033c05a35dff2303038ce9d8624be596
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-20 17:35:45 +0530

    Auto-commit

--- 2024 - 08 - Aug/Recursion/quick_sort.py
+++ 2024 - 08 - Aug/Recursion/quick_sort.py
@@ -1,8 +1,14 @@
-def partition():
+def partition(arr):
+    i = 0
+    h = len(arr)
+    pivot = arr[0]
+
     return
 
 # def quick_sort(arr):
 #     return
 
 arr = [2,6,8,9,4]
-quick_sort(arr)
+low = 0
+high = len(arr)-1
+quick_sort(arr,low,high)
