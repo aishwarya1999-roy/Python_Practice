0bc265395539fee8effdfcb23cf830b9d9b13775
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-20 09:12:14 +0530

    Auto-commit

--- 2024 - 08 - Aug/Recursion/merge_sort.py
+++ 2024 - 08 - Aug/Recursion/merge_sort.py
@@ -3,12 +3,10 @@ def merge_sort(arr, n,s,e):
         return
     mid = (s+e)//2
     print(arr[mid])
-    left = merge_sort(arr,n-1,s,mid+1)
-    right = merge_sort(arr,n-1,mid+1,e)
-    print("left: ", left)
-    print("right: ", right)
-    
+    merge_sort(arr,n-1,s,mid+1)
+    merge_sort(arr,n-1,mid+1,e)
 
 arr = [55, 16, 9, 23, 3, 12, 3]
     #   0   1  2  3   4   5  6
-merge_sort(arr, 7,0,7)
+n = len(arr)
+merge_sort(arr, 0,n-1)
