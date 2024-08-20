fa595792d1a0717cf75063eb8f7b4e5a502c08b9
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-20 08:38:27 +0530

    Auto-commit

--- 2024 - 08 - Aug/Recursion/merge_sort.py
+++ 2024 - 08 - Aug/Recursion/merge_sort.py
@@ -1,13 +1,7 @@
 def merge_sort(arr, n):
-    if n == 0 or n == 1:
-        return
-    for i in range(n-1):
-        if arr[i]>arr[i+1]:
-            arr[i],arr[i+1]=arr[i+1], arr[i] 
-    merge_sort(arr, n-1)
+    
+    return
 
 arr = [55,16,9,23,3,12,3]
 n = len(arr)
 merge_sort(arr, n)
-for i in range(n):
-    print(arr[i], end=" ")
