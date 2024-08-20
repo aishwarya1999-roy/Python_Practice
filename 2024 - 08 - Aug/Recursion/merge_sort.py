b9bea1671ae921ce33c84538326d2787a856f80b
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-20 09:54:09 +0530

    Auto-commit

--- 2024 - 08 - Aug/Recursion/merge_sort.py
+++ 2024 - 08 - Aug/Recursion/merge_sort.py
@@ -1,29 +1,8 @@
 def merge(arr,mid,s,e):
-    mid=(s+e)//2
     left = arr[s:mid+1]
     right = arr[mid+1:e+1]
     i= j = 0
     k = s
-    for i in range(size1):
-        left[i] = arr[k]
-        k+=1
-
-    k = mid+1
-    for i in range(size2):
-        right[i] = arr[k]
-        k+=1
-    ind1 = 0
-    ind2 = 0
-    k = s   
-    while ind1<size1 and ind2<size2:
-        if (left[ind1]<right[ind2]):
-            arr[k] = left[ind1]
-            k+=1
-            ind1+=1
-        else:
-            arr[k] = right[ind2]
-            k+=1
-            ind2+=1
 
 def merge_sort(arr, s,e):
     if s>=e:
