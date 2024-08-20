0f969423e027bfdbdd484342aa6d4b30b19c595d
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-20 08:34:59 +0530

    Auto-commit

---
+++ 2024 - 08 - Aug/Recursion/merge_sort.py
@@ -0,0 +1,12 @@
+def merge_sort(arr, n):
+    if n == 0 or n == 1:
+        return
+    for i in range(n-1):
+        if arr[i]>arr[i+1]:
+            arr[i],arr[i+1]=arr[i+1], arr[i] 
+    bubble_sort(arr, n-1)
+arr = [5,1,9,2,3]
+n = len(arr)
+bubble_sort(arr, n)
+for i in range(n):
+    print(arr[i], end=" ")
