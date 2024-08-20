7b15f5a2a73d7d7cde6bd86fca41967b72893656
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-20 08:07:32 +0530

    Auto-commit

--- 2024 - 08 - Aug/Recursion/bubble_sort.py
+++ 2024 - 08 - Aug/Recursion/bubble_sort.py
@@ -1,10 +1,10 @@
 def bubble_sort(arr, n):
     if n == 0 or n == 1:
-        return arr
+        return
     for i in range(n-1):
         if arr[i]>arr[i+1]:
             arr[i],arr[i+1]= arr[i], arr[i+1]
-    bubble_sort(arr, n-1)
+    return bubble_sort(arr, n-1)
 
 arr = [5,1,9,2,3]
 n = len(arr)
