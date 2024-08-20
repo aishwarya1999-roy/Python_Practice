ad720b4ef58199fbbcf64ae6f474c8f2ad51e754
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-20 09:55:23 +0530

    Auto-commit

--- 2024 - 08 - Aug/Recursion/merge_sort.py
+++ 2024 - 08 - Aug/Recursion/merge_sort.py
@@ -10,7 +10,16 @@ def merge(arr,mid,s,e):
         else:
             arr[k] = right[j]
             j += 1
-        k += 1   
+        k += 1
+
+    while i < len(left):
+        arr[k] = left[i]
+        i += 1
+        k += 1
+    while j < len(right):
+        arr[k] = right[j]
+        j += 1
+        k += 1
 def merge_sort(arr, s,e):
     if s>=e:
         return
