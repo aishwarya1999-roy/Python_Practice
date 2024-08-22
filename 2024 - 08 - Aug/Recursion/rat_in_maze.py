a6127f56a03e12108344b85858a2d04f8e6445df
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-22 18:59:02 +0530

    Auto-commit

--- 2024 - 08 - Aug/Recursion/rat_in_maze.py
+++ 2024 - 08 - Aug/Recursion/rat_in_maze.py
@@ -5,6 +5,8 @@ def solve():
 def rat(m,n):
     ans = ''
     srcX = srcY = 0
+    if m[0][0]==0:
+        return []
     visited = [[0 for _ in range(n)] for _ in range(4)]
     path = ''
     solve(m,n,ans,srcX,srcY,visited,path)
