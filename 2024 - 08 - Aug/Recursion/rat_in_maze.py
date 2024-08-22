4047a00cba13f25cd6aa79d1ec6952eab379693d
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-22 18:51:14 +0530

    Auto-commit

--- 2024 - 08 - Aug/Recursion/rat_in_maze.py
+++ 2024 - 08 - Aug/Recursion/rat_in_maze.py
@@ -5,12 +5,12 @@ m = [[1,0,0,0],
      [1,1,0,1],
      [1,1,0,0],
      [0,1,1,1]]
-# n = len(m)
+n = len(m)
 # ans = ''
 # srcX = srcY = 0
 visited = m
-for i in range(4):
-    for j in range(4):
+for i in range(n):
+    for j in range(n):
         visited[i][j]=0
 print(visited)
 
