5611afef0c29acaccd9e0fcabeee03a5dfeb758c
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-22 16:25:34 +0530

    Auto-commit

---
+++ 2024 - 08 - Aug/Recursion/permutation_of_string.py
@@ -0,0 +1,9 @@
+def solve(digits,output,index, ans, mapping):
+    return 
+
+digit = "abc"
+mapping = [' ', ' ', 'abc','def','ghi','jkl','mno','pqrs','tuv','wxyz']
+output= []
+ans = []
+solve(digit,output, 0, ans, mapping)
+print(ans)
