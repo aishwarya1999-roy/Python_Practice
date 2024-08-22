c93803da72dbadd124e53aedebea83e11a90c0f8
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-22 19:04:16 +0530

    Auto-commit

--- 2024 - 08 - Aug/Recursion/rat_in_maze.py
+++ 2024 - 08 - Aug/Recursion/rat_in_maze.py
@@ -1,10 +1,17 @@
+
+def issafe(x,y,n,visited,m):
+
 def solve(m,n,ans,x,y,visited,path):
     if x == n-1 and y == n-1:
         ans.append(''.join(path[:]))
         return
     
     visited[x][y] = 1
+    #down
+    newx= x+1
+    newy= x-1
 
+    if 
 
 
 
