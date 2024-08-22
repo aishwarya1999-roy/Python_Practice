57bb67702437857838e56b1f60a47d56f1e3f039
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-22 17:43:43 +0530

    Auto-commit

--- 2024 - 08 - Aug/Recursion/permutation_of_string.py
+++ 2024 - 08 - Aug/Recursion/permutation_of_string.py
@@ -1,22 +1,12 @@
-# def solve(str1,output,index, ans):
-#     if index>=len(str1):
-#         ans.append(''.join(output[:]))
-#         return
+def solve(str1,output,index, ans):
+    if index>=len(str1):
+        ans.append(''.join(output[:]))
+        return
     
 
 
-# str1 = "abc"
-# output= []
-# ans = []
-# solve(str1,output, 0, ans)
-# print(ans)
-
-
-
-str1 = ['a','b','c']
-aa = str1[:]
-ind = 0
-for i in str1:
-    if str1.index(i) > 0:
-        aa[str1.index(i)],aa[0] = aa[0], aa[str1.index(i)]
-    print(''.join(str1))
+str1 = "abc"
+output= []
+ans = []
+solve(str1,output, 0, ans)
+print(ans)
