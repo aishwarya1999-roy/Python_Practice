d52cfa22c717b1480535b24ea843f8325639cb23
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-22 18:50:01 +0530

    Auto-commit

--- 2024 - 08 - Aug/Recursion/rat_in_maze.py
+++ 2024 - 08 - Aug/Recursion/rat_in_maze.py
@@ -1,13 +1,13 @@
-def solve():
-    return
+# def solve():
+#     return
 
-m = [[1,0,0,0],
-     [1,1,0,1],
-     [1,1,0,0],
-     [0,1,1,1]]
-n = len(m)
-ans = ''
-srcX = srcY = 0
+# m = [[1,0,0,0],
+#      [1,1,0,1],
+#      [1,1,0,0],
+#      [0,1,1,1]]
+# n = len(m)
+# ans = ''
+# srcX = srcY = 0
 visited = [[]]
 for i in range(4):
     for j in range(4):
