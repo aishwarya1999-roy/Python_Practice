d7ecf9aeef2c1c1908e64aa7ce219a6840141f1c
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-22 17:54:24 +0530

    Auto-commit

--- 2024 - 08 - Aug/Recursion/permutation_of_string.py
+++ 2024 - 08 - Aug/Recursion/permutation_of_string.py
@@ -1,6 +1,6 @@
 def solve(nums,ind,ans):
     if ind>=len(nums):
-        ans.append(nums)
+        ans.append(nums[:])
         return
     for i in range(ind, len(nums)):
         nums[ind],nums[i] = nums[i],nums[ind] 
