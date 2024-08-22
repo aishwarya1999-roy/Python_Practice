01f361e3bdefe3f6baf39fdb2d25d33927921697
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-22 17:21:39 +0530

    Auto-commit

--- 2024 - 08 - Aug/Recursion/permutation_of_string.py
+++ 2024 - 08 - Aug/Recursion/permutation_of_string.py
@@ -1,15 +1,12 @@
-# def solve(str1,output,index, ans):
-#     if index>=len(str1):
-#         ans.append(''.join(output[:]))
-#         return
+def solve(str1,output,index, ans):
+    if index>=len(str1):
+        ans.append(''.join(output[:]))
+        return
     
 
 
-# str1 = "abc"
-# output= []
-# ans = []
-# solve(str1,output, 0, ans)
-# print(ans)
-
-
-
+str1 = "abc"
+output= []
+ans = []
+solve(str1,output, 0, ans)
+print(ans)
