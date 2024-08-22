4e0e1a4d5bc62945703aa36ff2d00ef2aa3fb8ab
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-22 18:54:11 +0530

    Auto-commit

--- 2024 - 08 - Aug/Recursion/rat_in_maze.py
+++ 2024 - 08 - Aug/Recursion/rat_in_maze.py
@@ -9,7 +9,13 @@ n = len(m)
 # ans = ''
 # srcX = srcY = 0
 visited = [[0 for _ in range(4)] for _ in range(4)]
+
+for i in range(4):
+    for j in range(4):
+        visited[i][j] = 0
+
 print(visited)
+
 
 
 
